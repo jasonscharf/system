fa4eceea7d1ace724bb2ab905def36368b73c190
jason <jasonlscharf@gmail.com>
2026-01-01 22:15:18 -0800

    Add test packages w/ vitest

---
+++ packages/test/src/e2e/e2e-example.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+
+test('can navigate to about', async ({ page }) => {
+    await page.goto('/');
+    await page.click('text=About');
+    await expect(page).toHaveURL(/about/);
+});

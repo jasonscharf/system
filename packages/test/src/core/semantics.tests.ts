f0fed724b148006de09bf4e809ac80e2ac03518d
jason <jasonlscharf@gmail.com>
2026-01-02 15:21:55 -0800

    Tidy semantics suite

--- packages/test/src/core/semantics.tests.ts
+++ packages/test/src/core/semantics.tests.ts
@@ -3,15 +3,11 @@ import { IRI } from '@system/core';
 import { describe, it, expect } from 'vitest';
 
 
-describe('core', () => {
-    describe('semantics', () => {
-        describe('IRI', () => {
-            it('constructs from a valid URI', () => {
-                const uri = 'http://example.com';
-                const iri = new IRI(uri);
-                expect(iri.toString()).to.eq(uri);
-                expect(iri.toJSON()).to.eq(uri);
-            });
-        })
+describe('IRI', () => {
+    it('constructs from a valid URI', () => {
+        const uri = 'http://example.com';
+        const iri = new IRI(uri);
+        expect(iri.toString()).to.eq(uri);
+        expect(iri.toJSON()).to.eq(uri);
     });
 });

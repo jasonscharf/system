ac545de9732efbeff4b4aedefda6e53854405bc1
jason <jasonlscharf@gmail.com>
2026-01-01 22:15:00 -0800

    Add initial core code

---
+++ packages/core/src/util/binary.ts
@@ -0,0 +1,10 @@
+/**
+ * Converts a byte array to hex.
+ * @param bytes 
+ * @returns 
+ */
+export function uint8ToHex(bytes: Uint8Array): string {
+    return Array.from(bytes)
+        .map(b => b.toString(16).padStart(2, "0"))
+        .join("");
+}
